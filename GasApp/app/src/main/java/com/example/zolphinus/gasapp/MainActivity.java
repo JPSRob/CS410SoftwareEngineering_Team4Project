bd652ba3ead96170356387ec9b5ea6c0387fae82
zolphinus <zolphinus@yahoo.com>
2015-02-15 19:36:14 -0500

    ListView can now pull JSON tutorial data

--- GasApp/app/src/main/java/com/example/zolphinus/gasapp/MainActivity.java
+++ GasApp/app/src/main/java/com/example/zolphinus/gasapp/MainActivity.java
@@ -1,19 +1,157 @@
 package com.example.zolphinus.gasapp;
 
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
+
+    private ProgressDialog pDialog;
+
+    //url to get JSON info
+    private static String url = "http://api.androidhive.info/contacts/";
+
+
+
+    //JSON Node labels
+    private static final String TAG_CONTACT = "contacts";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_EMAIL = "email";
+    private static final String TAG_ADDRESS = "address";
+    private static final String TAG_GENDER = "gender";
+    private static final String TAG_PHONE = "phone";
+    private static final String TAG_PHONE_MOBILE = "mobile";
+    private static final String TAG_PHONE_HOME = "home";
+    private static final String TAG_PHONE_OFFICE = "office";
+
+    JSONArray contacts = null;
+    ArrayList<HashMap<String, String>> contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.
+        ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        contactList = new ArrayList<HashMap<String,String>>();
+
+        ListView lv = getListView();
+        new GetContacts().execute();
     }
 
+
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            //pDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            ServiceHandler sh = new ServiceHandler();
+
+            String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
+
+            Log.d("Reponse: ", "> " + jsonStr);
+
+            if(jsonStr != null){
+                try{
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    //grab array node
+                    contacts = jsonObj.getJSONArray(TAG_CONTACT);
+
+                    //loop through array
+                    for(int i = 0; i < contacts.length(); i++){
+                        //grabs first object in array
+                        JSONObject c = contacts.getJSONObject(i);
+
+                        //grab object fields and assign to strings
+
+                        String id = c.getString(TAG_ID);
+                        String name = c.getString(TAG_NAME);
+                        String email = c.getString(TAG_EMAIL);
+                        String address = c.getString(TAG_ADDRESS);
+                        String gender = c.getString(TAG_GENDER);
+
+                        //grab JSON node from within the current JSON object
+
+                        JSONObject phone = c.getJSONObject(TAG_PHONE);
+                        String mobile = phone.getString(TAG_PHONE_MOBILE);
+                        String home = phone.getString(TAG_PHONE_HOME);
+                        String office = phone.getString(TAG_PHONE_OFFICE);
+
+                        //create temporary hash map for single entry
+                        HashMap<String, String> contact = new HashMap<String, String>();
+
+                        //adds child nodes into HashMap   key/value
+                        contact.put(TAG_ID, id);
+                        contact.put(TAG_NAME, name);
+                        contact.put(TAG_EMAIL, email);
+                        contact.put(TAG_PHONE_MOBILE, mobile);
+
+                        //then add to the entry list
+                        contactList.add(contact);
+                    }
+
+                } catch (JSONException e){
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("ServiceHandler", "Couldn't get any data from the url");
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result){
+            super.onPostExecute(result);
+            //cleanup progress dialog
+            if(pDialog != null){
+                if(pDialog.isShowing())
+                    pDialog.dismiss();
+                pDialog = null;
+            }
+
+
+            //Update JSON into ListView
+
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, contactList,
+                    R.layout.list_item, new String[] {TAG_NAME, TAG_EMAIL,
+                    TAG_PHONE_MOBILE }, new int[] {R.id.name,
+                    R.id.email, R.id.mobile });
+
+            setListAdapter(adapter);
+
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
