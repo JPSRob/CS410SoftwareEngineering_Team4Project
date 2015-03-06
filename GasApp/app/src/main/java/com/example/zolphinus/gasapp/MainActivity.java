5f157abbd9d4b247db5b42804b538b9915d20aeb
zolphinus <zolphinus@yahoo.com>
2015-03-05 23:29:40 -0500

    Added Strict Mode

--- GasApp/app/src/main/java/com/example/zolphinus/gasapp/MainActivity.java
+++ GasApp/app/src/main/java/com/example/zolphinus/gasapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
