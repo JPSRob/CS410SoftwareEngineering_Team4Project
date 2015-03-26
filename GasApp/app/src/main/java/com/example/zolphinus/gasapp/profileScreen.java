d2ee5424003d1583d745b32bc5eec2bf24d4fec1
JPSRob <jps.roberts@gmail.com>
2015-03-26 01:04:20 -0400

    profileScreen save functionality
    Created code for saving profile information to an internal file. This
    will create the file if it does not already exist, and append all
    entries to the end of the file. The file entries will be a single string
    of profileName concatenated with MPG, delimited by a comma.
    
    Exception throwing has been included in all needed function calls.
    There is a snippet of code for testing purposes, that should display a
    toast with file information, after saving and exiting the
    activity_profile_screen.
    
    BUGS: The toast for testing file contents is displaying numbers, rather
    than the String that should be saving to the file.

--- GasApp/app/src/main/java/com/example/zolphinus/gasapp/profileScreen.java
+++ GasApp/app/src/main/java/com/example/zolphinus/gasapp/profileScreen.java
@@ -1,12 +1,23 @@
 package com.example.zolphinus.gasapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class profileScreen extends ActionBarActivity {
+
+    //Variables
+    private static String profileFileName = "Profiles.txt"; //File name
+    String profileInfo; //Variable for combining profile info to be written to file
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +28,58 @@ public class profileScreen extends ActionBarActivity {
     public void saveProfile(View v){
         //startActivity(new Intent(getApplicationContext(), MainActivity.class
         //));
+
+        //We will concatenate contents of profileNameTextEdit and MPGTextEdit with a ",' delimiter
+        profileInfo = (R.id.profileNameTextEdit) + "," + (R.id.MPGTextEdit);
+
+        FileOutputStream outputToFile = null;
+        try {
+            //MODE_APPEND will add to end of file, instead of overwriting the file
+            outputToFile = openFileOutput(profileFileName, Context.MODE_APPEND);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (outputToFile != null) {
+            try {
+                outputToFile.write(profileInfo.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //Close the file
+        try {
+            outputToFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //For testing: toast message of file contents
+        //*******************************************************************************
+        FileInputStream readFile = null;
+        try {
+            readFile = openFileInput(profileFileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String testReadText = "";
+        byte[] input = new byte[0];
+        try {
+            input = new byte[readFile.available()];
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            while(readFile.read(input) != -1) {}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        testReadText += new String(input);
+        Toast.makeText(getApplicationContext(), testReadText, Toast.LENGTH_LONG).show();
+        //*******************************************************************************
+
+        //Set view back to activity_main, then close this activity
         setContentView(R.layout.activity_main);
+        this.finish();
     }
 
     /*  public void onClick(View v){
