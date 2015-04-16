2063f61c8f0a4408b6e457a92ac655a8e065d2f6
JPSRob <jps.roberts@gmail.com>
2015-04-16 00:02:28 -0400

    Save screen: prevent blank spaces in fields
    Added code to prevent user from creating a new profile when either the
    Profile Name or MPG fields are left empty. A toast will inform the user
    of their error.

--- GasApp/app/src/main/java/com/example/zolphinus/gasapp/profileScreen.java
+++ GasApp/app/src/main/java/com/example/zolphinus/gasapp/profileScreen.java
@@ -30,66 +30,88 @@ public class profileScreen extends ActionBarActivity {
         //startActivity(new Intent(getApplicationContext(), MainActivity.class
         //));
 
+        //For testing with Toast
+        String testReadText = "";
+
         //We will concatenate contents of profileNameTextEdit and MPGTextEdit with a ",' delimiter
         EditText myProfileEdit = (EditText)findViewById(R.id.profileNameTextEdit);
         EditText myMPGEdit = (EditText)findViewById(R.id.MPGTextEdit);
-        profileInfo = (myProfileEdit.getText().toString()) + "," + (myMPGEdit.getText().toString() + "\n");
+        if (myProfileEdit.getText().toString().length() == 0 && myMPGEdit.getText().toString().length() == 0) {
 
-        FileOutputStream outputToFile = null;
-        try {
-            //MODE_APPEND will add to end of file, instead of overwriting the file
-            outputToFile = openFileOutput(profileFileName, Context.MODE_APPEND);
+            Toast.makeText(getApplicationContext(), "You left a field empty!", Toast.LENGTH_LONG).show();
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
-        if (outputToFile != null) {
+        else {
+
+            //Grab Profile Name and MPG from EditTexts in GUI
+            profileInfo = (myProfileEdit.getText().toString()) + "," + (myMPGEdit.getText().toString() + "\n");
+
+            FileOutputStream outputToFile = null;
             try {
-                outputToFile.write(profileInfo.getBytes());
+                //MODE_APPEND will add to end of file, instead of overwriting the file
+                outputToFile = openFileOutput(profileFileName, Context.MODE_APPEND);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            if (outputToFile != null) {
+                try {
+                    outputToFile.write(profileInfo.getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //Close the file
+            try {
+                outputToFile.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-        //Close the file
-        try {
-            outputToFile.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+
+            FileInputStream readFile = null;
+            try {
+                readFile = openFileInput(profileFileName);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            byte[] input = new byte[0];
+            try {
+                input = new byte[readFile.available()];
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                while (readFile.read(input) != -1) {
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //For testing with Toast
+            //testReadText += new String(input);
+
+            //Close the file
+            try {
+                readFile.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            //For testing: toast message of file contents
+            //*******************************************************************************
+            //Toast.makeText(getApplicationContext(), testReadText, Toast.LENGTH_LONG).show();
+            //*******************************************************************************
+
+            //Set view back to activity_main, then close this activity
+            setContentView(R.layout.activity_main);
+            this.finish();
+
         }
 
-        //For testing: toast message of file contents
-        //*******************************************************************************
-        FileInputStream readFile = null;
-        try {
-            readFile = openFileInput(profileFileName);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        String testReadText = "";
-        byte[] input = new byte[0];
-        try {
-            input = new byte[readFile.available()];
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            while(readFile.read(input) != -1) {}
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        testReadText += new String(input);
-        //Close the file
-        try {
-            readFile.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Toast.makeText(getApplicationContext(), testReadText, Toast.LENGTH_LONG).show();
-        //*******************************************************************************
-
-        //Set view back to activity_main, then close this activity
-        setContentView(R.layout.activity_main);
-        this.finish();
     }
 
     /*  public void onClick(View v){
